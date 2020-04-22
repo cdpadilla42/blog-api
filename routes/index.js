9a221eccf805363920661a236fdd92e2e17ec00c
Chris Padilla <cdpadilla42@gmail.com>
2020-04-22 12:43:54 -0500

    Add user controller and routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const postsRouter = require('./posts');
+const usersRouter = require('./users');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -8,5 +9,6 @@ router.get('/', function (req, res, next) {
 });
 
 router.use('/posts', postsRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
